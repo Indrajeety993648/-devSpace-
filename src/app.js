b84a16f369d2221bbe6189d7624b6aa77ade49e4
Indrajeety993648 <indrajeetyadav993648@gmail.com>
2024-12-01 17:33:23 +0530

    multiple  route  handlers

--- src/app.js
+++ src/app.js
@@ -2,16 +2,23 @@ const express =  require('express')
 const app  = express();
 
 
+
 //  app.use("/user", (req, res) => {
 //     res.send(" mai  Badshah hu ")
 //  })
+
 app.get("/user?id" , (req, res)  => {
+    console.log(req.query);
     res.send({firstName : " Indrajeet " , lastName : "  Yada"})
-})
+} ,  (req, res) => {
+    res.send("Data  Updated  SuccessFully")
+}) 
 
  app.post("/user", (req , res) => {
      res.send("Data Updated  Succesfully ");
  })
+
+ 
 // app.use("/hello" , (req, res) => {
 //     res.send("hello, I am listening to you ... ")
 // })
@@ -21,7 +28,10 @@ app.get("/user?id" , (req, res)  => {
 // app.use("/" , (req, res) => {
 //     res.send("hello, I am  Your  home  ")
 // })
+
  app.listen(3000 ,() => {
     console.log('Server is successfully running on port 3000');
  });
-  console.log('Server is running on port 3000');
+
+//   console.log('Server is running on port 3000');
+
