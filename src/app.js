3b3b093fdc68d06e5039a5e60fedadf1c90c1555
Indrajeety993648 <indrajeetyadav993648@gmail.com>
2024-12-01 17:39:12 +0530

    Playing  with multiple  route handlers

--- src/app.js
+++ src/app.js
@@ -7,18 +7,18 @@ const app  = express();
 //     res.send(" mai  Badshah hu ")
 //  })
 
-app.get("/user?id" , (req, res)  => {
-    console.log(req.query);
-    res.send({firstName : " Indrajeet " , lastName : "  Yada"})
-} ,  (req, res) => {
-    res.send("Data  Updated  SuccessFully")
-}) 
+// app.get("/user?id" , (req, res)  => {
+//     console.log(req.query);
+//     res.send({firstName : " Indrajeet " , lastName : "  Yada"})
+// } ,  (req, res) => {
+//     res.send("Data  Updated  SuccessFully")
+// }) 
 
- app.post("/user", (req , res) => {
-     res.send("Data Updated  Succesfully ");
- })
+//  app.post("/user", (req , res) => {
+//      res.send("Data Updated  Succesfully ");
+//  })
 
- 
+
 // app.use("/hello" , (req, res) => {
 //     res.send("hello, I am listening to you ... ")
 // })
@@ -29,6 +29,28 @@ app.get("/user?id" , (req, res)  => {
 //     res.send("hello, I am  Your  home  ")
 // })
 
+
+
+// we can  send multiple  route handlers  in a  single route  , we can send  some route 
+//  handlers  inside array  and some  outside array 
+
+
+// app.use("/user" , [rh1 , rh2 ], rh3, rh4 .....)
+ app.use(
+    "/user" ,
+     (req, res , next) =>{
+      console.log( " Route 1  is working ")
+      next();
+    //   res.send("route 1 is working ");
+    } ,
+    (req, res , next) => {
+         console.log(" Route 2 is working ");
+         next();
+    },
+    (req, res, next) => {
+        console.log(" Route 3 is working ");
+        res.send("Route 3 is working ");
+    })
  app.listen(3000 ,() => {
     console.log('Server is successfully running on port 3000');
  });
