af3e48a7670a1cdab4f59cc1fd0d7ab4016d1825
Indrajeety993648 <indrajeetyadav993648@gmail.com>
2024-12-01 14:02:30 +0530

    Playing  with Router

--- src/app.js
+++ src/app.js
@@ -2,9 +2,25 @@ const express =  require('express')
 const app  = express();
 
 
-app.use((req, res) => {
-    res.send("hello, I am listening to you ... ")
+//  app.use("/user", (req, res) => {
+//     res.send(" mai  Badshah hu ")
+//  })
+app.get("/user?id" , (req, res)  => {
+    res.send({firstName : " Indrajeet " , lastName : "  Yada"})
 })
+
+ app.post("/user", (req , res) => {
+     res.send("Data Updated  Succesfully ");
+ })
+// app.use("/hello" , (req, res) => {
+//     res.send("hello, I am listening to you ... ")
+// })
+// app.use("/test" , (req, res) => {
+//     res.send("hello, I am test ... ")
+// })
+// app.use("/" , (req, res) => {
+//     res.send("hello, I am  Your  home  ")
+// })
  app.listen(3000 ,() => {
     console.log('Server is successfully running on port 3000');
  });
